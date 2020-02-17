0dc5bec4b3a34aa6a2185f4d695eef82b656da3b
Jofreylin <trabajosjofrey@hotmail.com>
2020-02-17 17:23:43 -0400

    Added global variables

---
+++ src/app/logica/modify.class.ts
@@ -0,0 +1,17 @@
+import { DecimalPipe } from '@angular/common';
+
+export class Modify{
+
+    code: string = '';
+    title: string = '';
+    quantity: number = 0;
+    price: number = 0.00;
+    category: string = '';
+    description: string = '';
+
+    constructor(){
+
+    }
+
+
+}
